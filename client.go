f5f386b42cf31bbbd13f709701f6e2742a0e4f1d
agaton <agaton.sjoberg@modularfinance.se>
2023-12-06 11:22:38 +0100

    Update client to handle multiple servers and upgrade to go 1.21

--- client.go
+++ client.go
@@ -5,44 +5,91 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/modfin/henry/slicez"
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
+	"sync"
+	"time"
 )
 
-func NewClient(apiKey string, host string) *Client {
-	host = strings.TrimRight(host, "/")
-	return &Client{
-		host:   host,
-		apiKey: apiKey,
+type Client interface {
+	Send(ctx context.Context, email *Email) (Receipt, error)
+}
+
+type TargetServer struct {
+	Host          string
+	MxCNAME       string
+	isInitialized bool
+	downUntil     time.Time
+}
+
+type client struct {
+	apiKey        string
+	domain        string
+	posthookUrl   string
+	maxRetires    int
+	targetServers []TargetServer
+	mu            sync.RWMutex
+}
+
+// New returns a new brev client that can be used to send emails across multiple brev servers
+// apiKey is the key used to gain access to the servers, domain is the domain which the emails will be sent from
+// posthookUrl will receive posts with information regarding the status of the sent email
+// target servers are the different brev servers that the client will use to send emails
+// if we are unable to send an email, maxRetries determines how many extra times we attempt to send the email
+func New(apiKey string, domain string, posthookUrl string, targetServers []TargetServer, maxRetries int) (Client, error) {
+	apiKey = strings.TrimSpace(apiKey)
+	domain = strings.TrimSpace(domain)
+	posthookUrl = strings.TrimSpace(posthookUrl)
+	if len(apiKey) == 0 {
+		return nil, fmt.Errorf("invalid api key")
 	}
+	if len(domain) == 0 {
+		return nil, fmt.Errorf("invalid domain")
+	}
+	_, err := url.Parse(posthookUrl)
+	if err != nil {
+		return nil, err
+	}
+	if len(targetServers) == 0 {
+		return nil, fmt.Errorf("at least one target server is required")
+	}
+	if maxRetries < 0 {
+		maxRetries = 0
+	}
+
+	for i, _ := range targetServers {
+		targetServers[i].Host = strings.TrimRight(targetServers[i].Host, "/")
+		targetServers[i].isInitialized = false
+		targetServers[i].downUntil = time.Now()
+	}
+	c := client{
+		apiKey:        apiKey,
+		domain:        domain,
+		posthookUrl:   posthookUrl,
+		targetServers: targetServers,
+		maxRetires:    maxRetries,
+	}
+	return &c, nil
 }
 
-type Client struct {
-	host   string
-	apiKey string
-}
-
-type Receipt struct {
-	MessageId      string  `json:"message_id"`
-	TransactionIds []int64 `json:"transaction_ids"`
-}
-
-func (c *Client) UpdateKeySettings(ctx context.Context, domain string, MxCNAME string, posthookUrl url.URL) error {
+func (c *client) updateKeySettings(ctx context.Context, targetServer TargetServer) error {
 	body, err := json.Marshal(struct {
 		Domain      string `json:"domain"`
 		MxCNAME     string `json:"mx_cname"`
 		PosthookURL string `json:"posthook_url"`
 	}{
-		Domain:      domain,
-		MxCNAME:     MxCNAME,
-		PosthookURL: posthookUrl.String(),
+		Domain:      c.domain,
+		MxCNAME:     targetServer.MxCNAME,
+		PosthookURL: c.posthookUrl,
 	})
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("PUT", c.host+"/key-settings?key="+c.apiKey, bytes.NewBuffer(body))
+	req, err := http.NewRequest("PUT", targetServer.Host+"/key-settings?key="+c.apiKey, bytes.NewBuffer(body))
 	if err != nil {
 		return err
 	}
@@ -56,17 +103,84 @@ func (c *Client) UpdateKeySettings(ctx context.Context, domain string, MxCNAME s
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
 		return fmt.Errorf("bad status: %d", resp.StatusCode)
 	}
+	// update isInitialized since we have successfully updated key settings
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.targetServers = slicez.Map(c.targetServers, func(ts TargetServer) TargetServer {
+		if ts.Host != targetServer.Host {
+			return ts
+		}
+		ts.isInitialized = true
+		return ts
+	})
 	return nil
 }
 
-func (c *Client) Send(ctx context.Context, email *Email) (Receipt, error) {
+type Receipt struct {
+	MessageId      string  `json:"message_id"`
+	TransactionIds []int64 `json:"transaction_ids"`
+}
 
+func (c *client) getServer() TargetServer {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	availableServers := slicez.Filter(c.targetServers, func(s TargetServer) bool {
+		return s.downUntil.Before(time.Now())
+	})
+	// if no servers are available, any server will be used
+	if len(availableServers) == 0 {
+		availableServers = c.targetServers
+	}
+	// return a random server
+	return availableServers[rand.Intn(len(availableServers))]
+}
+func (c *client) handleServerError(targetServer TargetServer) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	downUntil := time.Now().Add(time.Minute)
+	fmt.Printf("[BREV-CLIENT] unable to send emails from host %s flagging server as down until %s\n", targetServer.Host, downUntil.Format(time.RFC3339))
+	c.targetServers = slicez.Map(c.targetServers, func(ts TargetServer) TargetServer {
+		if ts.Host != targetServer.Host {
+			return ts
+		}
+		ts.downUntil = downUntil
+		return ts
+	})
+}
+
+func (c *client) Send(ctx context.Context, email *Email) (Receipt, error) {
 	body, err := json.Marshal(email)
 	if err != nil {
 		return Receipt{}, err
 	}
 
-	req, err := http.NewRequest("POST", c.host+"/mta?key="+c.apiKey, bytes.NewBuffer(body))
+	var receipt Receipt
+	for i := 0; i <= c.maxRetires; i++ {
+		select {
+		case <-ctx.Done():
+			return Receipt{}, ctx.Err()
+		default:
+		}
+		receipt, err = c.send(ctx, body)
+		if err == nil {
+			return receipt, nil
+		}
+	}
+
+	return receipt, err
+}
+
+func (c *client) send(ctx context.Context, body []byte) (Receipt, error) {
+	// decide which server to send to
+	server := c.getServer()
+	if !server.isInitialized {
+		err := c.updateKeySettings(ctx, server)
+		if err != nil {
+			return Receipt{}, err
+		}
+	}
+
+	req, err := http.NewRequest("POST", server.Host+"/mta?key="+c.apiKey, bytes.NewBuffer(body))
 	if err != nil {
 		return Receipt{}, err
 	}
@@ -75,6 +189,9 @@ func (c *Client) Send(ctx context.Context, email *Email) (Receipt, error) {
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return Receipt{}, err
+	}
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return Receipt{}, fmt.Errorf("bad status: %d", resp.StatusCode)
 	}
 	defer resp.Body.Close()
 	respBytes, err := io.ReadAll(resp.Body)
--- go.mod
+++ go.mod
@@ -1,54 +1,55 @@
 module github.com/modfin/brev
 
-go 1.19
+go 1.21
 
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/flashmob/go-guerrilla v1.6.1
 	github.com/go-test/deep v1.1.0
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.4.0
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/labstack/echo-contrib v0.14.1
-	github.com/labstack/echo/v4 v4.10.2
-	github.com/mattn/go-sqlite3 v1.14.16
-	github.com/modfin/henry v0.0.0-20220425073158-37972c80b10d
-	github.com/stretchr/testify v1.8.2
-	github.com/urfave/cli/v2 v2.23.2
-	golang.org/x/crypto v0.14.0
+	github.com/labstack/echo-contrib v0.15.0
+	github.com/labstack/echo/v4 v4.11.3
+	github.com/mattn/go-sqlite3 v1.14.18
+	github.com/modfin/henry v0.0.0-20230824150253-35f12224ee68
+	github.com/stretchr/testify v1.8.4
+	github.com/urfave/cli/v2 v2.26.0
+	golang.org/x/crypto v0.16.0
 )
 
 require (
 	github.com/asaskevich/EventBus v0.0.0-20200907212545-49d423059eef // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/labstack/gommon v0.4.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.14.0 // indirect
-	github.com/prometheus/client_model v0.3.0 // indirect
-	github.com/prometheus/common v0.40.0 // indirect
-	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/sirupsen/logrus v1.6.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
